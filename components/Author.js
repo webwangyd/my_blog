c9fd5d9086dc068610883071df968010308bf458
webwangyd <1131847259@qq.com>
2020-02-27 23:11:22 +0800

    开发博客列表页前端

---
+++ components/Author.js
@@ -0,0 +1,23 @@
+
+import {Avatar,Divider} from 'antd'
+import '../static/style/components/author.css'
+
+const Author =()=>{
+
+    return (
+        <div className="author-div comm-box">
+            <div> <Avatar size={100} src="../static/imag/myImage.png"  /></div>
+            <div className="author-introduction">
+                前端开发者, 热爱技术, 专注于WEB和移动前端开发。
+                <Divider>社交账号</Divider>
+                <Avatar size={28} icon="github" className="account"  />
+                <Avatar size={28} icon="qq"  className="account" />
+                <Avatar size={28} icon="wechat"  className="account"  />
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Author
